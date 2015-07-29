3f151e33a7b46228a700f2655be5ce57b77c06ad
Christian Kniep <ckniep@gaikai.com>
2015-07-29 16:49:25 +0200

    RANDOM_METRICS create mymetric.test

---
+++ opt/qnib/grafana/bin/feed_random_metrics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+METRIC=${1-mymetric.test}
+SLEEP=${1-5}
+
+if [ "X${RANDOM_METRICS}" != "Xtrue" ];then
+   echo "RANDOM_METRICS is not set to true, therefore I skip this"
+   return 0
+fi
+
+while [ true ];do 
+    echo "put ${METRIC} $(date +%s) ${RANDOM} host=grafana"|nc -w1 opentsdb.service.consul 4242
+    sleep ${SLEEP}
+done
